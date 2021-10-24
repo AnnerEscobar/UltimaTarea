1f5c7ba6951c7f3fdeacba25ef924cd223a625b7
ANNER RONALDO ESCOBAR CRUZ <anner123escobar@gmail.com>
2021-10-23 21:20:23 -0600

    subiendo

---
+++ src/test/java/gtumg/demoumg/DemoumgApplicationTests.java
@@ -0,0 +1,13 @@
+package gtumg.demoumg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoumgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
